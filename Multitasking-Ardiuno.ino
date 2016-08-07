cdc3a7008807189350eb38ada49a8eed500ec058
Alexey-Tkachenko <alex_tk@tut.by>
2016-08-07 11:30:47 +0300

    Simple (too bad yet) task example

--- Multitasking-Ardiuno.ino
+++ Multitasking-Ardiuno.ino
@@ -1,13 +1,36 @@
+#include "Multitasking/Multitasking.h"
+
+class BlinkerTask : public TaskBase
+{
+private:
+	byte pin;
+	bool state;
+public:
+	explicit BlinkerTask(byte pin)
+		: TaskBase(), pin(pin), state()
+	{
+		pinMode(pin, OUTPUT);
+	}
+
+	virtual bool Step() override 
+	{
+		delay(100);
+		digitalWrite(pin, state = !state);
+		return false;
+	}
+};
+
 void setup()
 {
+	Scheduler<16> scheduler;
 
-  /* add setup code here */
+	BlinkerTask blinker(13);
 
+	scheduler.AddTask(&blinker);
+
+	scheduler.Run();
 }
 
 void loop()
 {
-
-  /* add main program code here */
-
 }
